31f48120ca686793ba15db1c9741d4fb348e25ed
Dishita Chaturvedi <dishitachaturvedi2005@gmail.com>
2026-09-12 22:38:46 +0530

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int maxi=0;
+        int mini=prices[0];
+        for(int& sell:prices){
+            maxi=max(maxi,sell-mini);
+            mini=min(mini,sell);
+        }
+        return maxi;
+    }
+};
